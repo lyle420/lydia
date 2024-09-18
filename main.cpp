b02ec3a92dd0f83151b86744421008d3e553694f
lydia <lydia.ochalla@strathmore.edu>
2024-09-18 09:33:42 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int marks;
+    cout<<"Enter student marks\n"<<endl;
+    cin>>marks;
+    if (marks>=50)
+        cout<<"The student has passed\n"<<endl;
+
+    return 0;
+}
